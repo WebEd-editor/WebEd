88523ed2f81fe45c7de9c4f787e41cb01d61f5cd
WebEd-editor <sahilkamatkar2006@gmail.com>
2025-07-22 16:59:08 +0530

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "WebEd - Website Builder",
+  "short_name": "WebEd",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#1e1e1e",
+  "icons": [
+    {
+      "src": "logo.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
