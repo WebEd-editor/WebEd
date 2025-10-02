4267fcc9a81be35237d29c5b03b541b2855c489d
WebEd-editor <sahilkamatkar2006@gmail.com>
2025-10-02 01:00:39 -0700

    Update webDekstop.js

--- webDekstop.js
+++ webDekstop.js
@@ -1,5 +1,12 @@
-function dek(){
+(() => {
   if(1020 < screen.width){
+  	MyOpen = function (id) {
+  		document.getElementById(id).style.display = "block";
+  		if(id === 'sidebar') updateTree();
+  	}
+  	startFloatDrag = function () {
+  		console.log('d3d3d3d3d3');
+  	}
 		let at = document.querySelectorAll('.tab');
 		at.forEach(allt => {
 			allt.classList.remove('drag-el');
@@ -10,20 +17,43 @@ function dek(){
 			allt.style.resize = "none";
 		});
 		document.querySelector("#editor-wrapper").style.display = "block";
-		document.querySelector("#sidebar").style.display = "block";
-	    document.querySelector("#sidebar").style.right = "5px";
+		const s = document.querySelector("#sidebar");
+		s.style.display = "block"; //sidebar tree, editor-wrapper add elements
+	  s.style.left = "80%"; 
 		document.querySelectorAll('summary').forEach(s => {s.style.fontSize = "13px";})
 		document.querySelector('#editor-wrapper').style.left = "10px";
 		const footer = document.querySelector('#foo');
-		footer.style = "position: fixed;bottom: 0;left: 50%;z-index: 102;transform: translateX(-50%);";
+		footer.style = "position: fixed;left: 0;top: 50%;z-index: 102;transform: translateY(-50%);display:flex;flex-direction: column;";
 		footer.classList.remove('drag-box');
 		footer.querySelectorAll('button').forEach(f => {
 			f.style.padding = "10px"
 			f.style.cursor = "pointer";
 		});
+	}
+})();
 
-		// preview
-		document.querySelector('ppp').style = "background: #000;position: fixed;bottom: 15px;left: 50%;transform: translateX(-50%);height: 90%;width: 25%;overflow: auto;border-radius: 5px;resize: both;border: 2px solid #222;"
+function preDec() {
+	if(1020 < screen.width){
+			document.querySelector('ppp').style = "background: #000;position: fixed;bottom: 15px;left: 50%;transform: translateX(-50%);height: 90%;width: 25%;overflow: auto;border-radius: 5px;resize: both;border: 2px solid #222;"
 	}
 }
-dek();
+preDec();
+
+// dekstop header
+function headDec(){
+	 const h = document.querySelector('.headDec');
+	 h.style = "border-bottom: 1px solid #000;z-index:102;display: flex;justify-content: space-between;position: fixed; top: 0; background: #1c2644;color: white; width: 100%; padding: 5px;";
+}
+headDec();
+
+//preview converter
+function precon(w){
+	 const p = document.querySelector('ppp');
+	 if(w === "1024"){
+	 		p.style.width = "1024px";
+	 }else if(w === "768"){
+	 		p.style.width = "768px";
+	 }else if(w === "320"){
+	 		p.style.width = "320px";
+	 }
+}
