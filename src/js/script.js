3de909af0c8ae650fe949e90609528a0b020c678
WebEd-editor <sahilkamatkar2006@gmail.com>
2026-03-15 07:50:02 -0700

    Clean up unused responsive variables in script.js
    Removed unused responsive arrays for different breakpoints.

--- src/js/script.js
+++ src/js/script.js
@@ -856,11 +856,7 @@ document.getElementById("respMode").addEventListener('input', ()=>{
    let m = document.getElementById('respMode').value;
    precon(m);
 });
-
-let resp1024 = [];
-let resp768 = [];
-let resp320 = [];
-let sthover = [];
+////rest var
 
 function generateResponsiveCSS(data, maxWidth) {
   let css = `/* webed responsive styles */\n@media (max-width: ${maxWidth}px) {\n`;
